5f21f34eb49d018c568252c9ab18fcf50063d57b
Steve <sgeiger5516@gmail.com>
2023-02-21 18:27:19 -0500

    setup maven and spring boot, added maven frontend plugin

---
+++ src/main/java/com/geistnine/OrderOut/OrderOutApplication.java
@@ -0,0 +1,13 @@
+package com.geistnine.OrderOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geistnine/OrderOut/OrderOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geistnine.OrderOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
